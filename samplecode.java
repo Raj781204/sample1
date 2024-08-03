68961a2c5b0a8b68e7991d2e0bb1bd8389af2f41
root <root@ip-172-31-4-146.ap-southeast-2.compute.internal>
2024-08-03 08:41:53 +0000

    First commit - login page code

---
+++ samplecode.java
@@ -0,0 +1,26 @@
+body {
+  font-family: "Poppins", sans-serif;
+  color: #040000;
+  background-color: #63f74f;
+}
+
+.layout_padding {
+  padding-top: 120px;
+  padding-bottom: 120px;
+}
+
+.layout_padding2 {
+  padding: 45px 0;
+}
+
+.layout_padding2-top {
+  padding-top: 45px;
+}
+
+.layout_padding2-bottom {
+  padding-bottom: 45px;
+}
+
+.layout_padding-top {
+  padding-top: 120px;
+}
